1b3115681e910b45b9b3644fa1b3846b67567231
visser <visser@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-26 11:36:27 +0000

    * Changed: Updated overview screen
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-exporter/trunk@997155 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/admin/tabs-overview.php
+++ templates/admin/tabs-overview.php
@@ -39,6 +39,10 @@
 			<a href="<?php echo add_query_arg( 'tab', 'export' ); ?>#export-product_vendor"><?php _e( 'Export Product Vendors', 'woo_ce' ); ?></a>
 			<span class="description">(<?php printf( __( 'available in %s', 'woo_ce' ), $woo_cd_link ); ?>)</span>
 		</li>
+		<li>
+			<a href="<?php echo add_query_arg( 'tab', 'export' ); ?>#export-shipping_class"><?php _e( 'Export Shipping Classes', 'woo_ce' ); ?></a>
+			<span class="description">(<?php printf( __( 'available in %s', 'woo_ce' ), $woo_cd_link ); ?>)</span>
+		</li>
 <!--
 		<li>
 			<a href="<?php echo add_query_arg( 'tab', 'export' ); ?>#export-attribute"><?php _e( 'Export Attributes', 'woo_ce' ); ?></a>
@@ -52,6 +56,26 @@
 
 	<h3><div class="dashicons dashicons-admin-settings"></div>&nbsp;<a href="<?php echo add_query_arg( 'tab', 'settings' ); ?>"><?php _e( 'Settings', 'woo_ce' ); ?></a></h3>
 	<p><?php _e( 'Manage export options from a single detailed screen.', 'woo_ce' ); ?></p>
+	<ul class="ul-disc">
+		<li>
+			<a href="<?php echo add_query_arg( 'tab', 'settings' ); ?>#general-settings"><?php _e( 'General Settings', 'woo_ce' ); ?></a>
+		</li>
+		<li>
+			<a href="<?php echo add_query_arg( 'tab', 'settings' ); ?>#csv-settings"><?php _e( 'CSV Settings', 'woo_ce' ); ?></a>
+		</li>
+		<li>
+			<a href="<?php echo add_query_arg( 'tab', 'settings' ); ?>#xml-settings"><?php _e( 'XML Settings', 'woo_ce' ); ?></a>
+			<span class="description">(<?php printf( __( 'available in %s', 'woo_ce' ), $woo_cd_link ); ?>)</span>
+		</li>
+		<li>
+			<a href="<?php echo add_query_arg( 'tab', 'settings' ); ?>#scheduled-exports"><?php _e( 'Scheduled Exports', 'woo_ce' ); ?></a>
+			<span class="description">(<?php printf( __( 'available in %s', 'woo_ce' ), $woo_cd_link ); ?>)</span>
+		</li>
+		<li>
+			<a href="<?php echo add_query_arg( 'tab', 'settings' ); ?>#cron-exports"><?php _e( 'CRON Exports', 'woo_ce' ); ?></a>
+			<span class="description">(<?php printf( __( 'available in %s', 'woo_ce' ), $woo_cd_link ); ?>)</span>
+		</li>
+	</ul>
 
 	<h3><div class="dashicons dashicons-hammer"></div>&nbsp;<a href="<?php echo add_query_arg( 'tab', 'tools' ); ?>"><?php _e( 'Tools', 'woo_ce' ); ?></a></h3>
 	<p><?php _e( 'Export tools for WooCommerce.', 'woo_ce' ); ?></p>
@@ -77,9 +101,12 @@
 		<li><?php _e( 'Export Orders', 'woo_ce' ); ?></li>
 		<li><?php _e( 'Export custom Order and Order Item meta', 'woo_ce' ); ?></li>
 		<li><?php _e( 'Export Customers', 'woo_ce' ); ?></li>
+		<li><?php _e( 'Export custom Customer meta', 'woo_ce' ); ?></li>
 		<li><?php _e( 'Export Coupons', 'woo_ce' ); ?></li>
+		<li><?php _e( 'Export custom User meta', 'woo_ce' ); ?></li>
 		<li><?php _e( 'Export Subscriptions', 'woo_ce' ); ?></li>
 		<li><?php _e( 'Export Product Vendors', 'woo_ce' ); ?></li>
+		<li><?php _e( 'Export Shipping Classes', 'woo_ce' ); ?></li>
 		<li><?php _e( 'CRON export engine', 'woo_ce' ); ?></li>
 		<li><?php _e( 'Schedule automatic exports with filtering options', 'woo_ce' ); ?></li>
 		<li><?php _e( 'Export to remote POST', 'woo_ce' ); ?></li>
